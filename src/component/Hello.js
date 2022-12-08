e5191e6a81a25284068db0956f238f1e1b03d505
Jun_Hyeok <ju36380@naver.com>
2022-12-08 13:56:26 +0900

    [update] state 속성값

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -3,15 +3,12 @@ import {useState} from "react";
 export default function Hello() {
     const [name, setName] = useState("Mike");
 
-    function changeName() {
-        const newName = name === "Mike" ? "Jane" : "Mike";
-        setName(newName);
-    }
     return (
         <div>
-            <h1>state</h1>
             <h2 id="name">{name}</h2>
-            <button onClick={changeName}>Change</button>
+            <button onClick={() =>{
+                setName(name === "Mike" ? "Jane" : "Mike");
+            }}>Change</button>
         </div>            
     );
 };
