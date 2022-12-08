175775e50da75f7a9d66a75f83073692ad5a2568
Jun_Hyeok <ju36380@naver.com>
2022-12-08 13:52:25 +0900

    [update] state 사용 (Hello.js)

---
+++ src/component/Hello.js
@@ -0,0 +1,17 @@
+import {useState} from "react";
+
+export default function Hello() {
+    const [name, setName] = useState("Mike");
+
+    function changeName() {
+        const newName = name === "Mike" ? "Jane" : "Mike";
+        setName(newName);
+    }
+    return (
+        <div>
+            <h1>state</h1>
+            <h2 id="name">{name}</h2>
+            <button onClick={changeName}>Change</button>
+        </div>            
+    );
+};
